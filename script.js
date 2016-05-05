4afae76c7e7d38dc875289636890871808c71c2b
Bogdan Linnik <bogdya.linnik@gmail.com>
2016-05-05 11:51:26 +0000

    Finished work on startpage

--- script.js
+++ script.js
@@ -5,24 +5,36 @@ $(document).ready(function() {
   $('body, header').addClass('startpage');
   $('.startpage').show();
   $('#stlinkcontacts').on('mouseenter', function() {
-    $(this).addClass('backgroundcontacts bgfa');
-    $('p.linktextc').remove();
-    $(this).animate({'bottom':'5%','right':'5%'},'slow');
+    $(this).toggleClass('backgroundcontacts bgfa').animate({'bottom':'5%','right':'5%'},'slow');
+    $('.linktextc').toggle('slow');
+  });
+  $('#stlinkcontacts').on('mouseleave', function() {
+    $(this).toggleClass('backgroundcontacts bgfa').animate({'bottom':'10%','right':'10%'},'slow');
+    $('.linktextc').toggle('slow');
   });
   $('#stlinkskills').on('mouseenter', function() {
-    $(this).addClass('backgroundskills bgfa');
-    $('p.linktexts').remove();
-    $(this).animate({'top':'5%','right':'5%'},'slow');
+    $(this).toggleClass('backgroundskills bgfa').animate({'top':'5%','right':'5%'},'slow');
+    $('.linktexts').toggle('slow');
+  });
+  $('#stlinkskills').on('mouseleave', function() {
+    $(this).toggleClass('backgroundskills bgfa').animate({'top':'10%','right':'10%'},'slow');
+    $('.linktexts').toggle('slow');
   });
   $('#stlinkme').on('mouseenter', function() {
-    $(this).addClass('backgroundsme bgfa');
-    $('p.linktextm').remove();
-    $(this).animate({'top':'5%','left':'5%'},'slow');
+    $(this).toggleClass('backgroundsme bgfa').animate({'top':'5%','left':'5%'},'slow');
+    $('.linktextm').toggle('slow');
+  });
+  $('#stlinkme').on('mouseleave', function() {
+    $(this).toggleClass('backgroundsme bgfa').animate({'top':'10%','left':'10%'},'slow');
+    $('.linktextm').toggle('slow');
   });
   $('#stlinkhobbies').on('mouseenter', function() {
-    $(this).addClass('backgroundshobbies bgfa');
-    $('p.linktexth').remove();
-    $(this).animate({'bottom':'5%','left':'5%'},'slow');
+    $(this).toggleClass('backgroundshobbies bgfa').animate({'bottom':'5%','left':'5%'},'slow');
+    $('.linktexth').toggle('slow');
+  });
+  $('#stlinkhobbies').on('mouseleave', function() {
+    $(this).toggleClass('backgroundshobbies bgfa').animate({'bottom':'10%','left':'10%'},'slow');
+    $('.linktexth').toggle('slow');
   });
     $('#stlinkme, #intlinkme, #clinkme, #sklinkme, #bklinkme').on('click', function() {
     $('.startpage, .intpage, .contact, .skillspage, .bookspage, .gamespage, .videospage, .audiospage').hide();
