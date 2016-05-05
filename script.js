d6bf5e5cc46f608c2435be2c533d878914325960
Bogdan Linnik <bogdya.linnik@gmail.com>
2016-05-05 07:11:14 +0000

    initial

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 var $;
 $(document).ready(function() {
   $('.mepage, .intpage, .contact, .skillspage, .bookspage , .gamespage, .videospage, .audiospage').hide();
+  $('body, header').removeClass('mepage intpage contact skillspage bookspage gamespage videospage audiospage');
+  $('body, header').addClass('startpage');
+  $('.startpage').show();
   $('#stlinkcontacts').on('mouseenter', function() {
     $(this).addClass('backgroundcontacts bgfa');
     $('p.linktextc').remove();
@@ -23,64 +26,64 @@ $(document).ready(function() {
   });
     $('#stlinkme, #intlinkme, #clinkme, #sklinkme, #bklinkme').on('click', function() {
     $('.startpage, .intpage, .contact, .skillspage, .bookspage, .gamespage, .videospage, .audiospage').hide();
-    $('body').removeClass('startpage intpage contact skillspage bookspage gamespage videospage audiospage');
-    $('body').addClass('mepage');
+    $('body, header').removeClass('startpage intpage contact skillspage bookspage gamespage videospage audiospage');
+    $('body, header').addClass('mepage');
     $('.mepage').show();
     $('title').text('Me');
   });
     $('#mlinkstart, #intlinkstart, #clinkstart, #sklinkstart, #bklinkstart').on('click', function() {
     $('.mepage, .intpage, .contact, .skillspage, .bookspage, .gamespage, .videospage, .audiospage').hide();
-    $('body').removeClass('mepage intpage contact skillspage bookspage gamespage videospage audiospage');
-    $('body').addClass('startpage');
+    $('body, header').removeClass('mepage intpage contact skillspage bookspage gamespage videospage audiospage');
+    $('body, header').addClass('startpage');
     $('.startpage').show();
     $('title').text('Start');
   });
     $('#stlinkhobbies, #mlinkhobbies, #clinkhobbies, #sklinkhobbies, #bklinkhobbies').on('click', function() {
     $('.startpage, .mepage, .contact, .skillspage, .bookspage, .gamespage, .videospage, .audiospage').hide();
-    $('body').removeClass('mepage startpage contact skillspage bookspage gamespage videospage audiospage');
-    $('body').addClass('intpage');
+    $('body, header').removeClass('mepage startpage contact skillspage bookspage gamespage videospage audiospage');
+    $('body, header').addClass('intpage');
     $('.intpage').show();
     $('title').text('Interests');
   });
   $('#stlinkcontacts, #mlinkcontacts, #intlinkcontacts, #sklinkcontacts, #bklinkcontacts').on('click', function() {
     $('.startpage, .intpage, .mepage, .skillspage, .bookspage, .gamespage, .videospage, .audiospage').hide();
-    $('body').removeClass('mepage intpage skillspage startpage bookspage gamespage videospage audiospage');
-    $('body').addClass('contact');
+    $('body, header').removeClass('mepage intpage skillspage startpage bookspage gamespage videospage audiospage');
+    $('body, header').addClass('contact');
     $('.contact').show();
     $('title').text('Contacts');
   });
   $('#stlinkskills, #mlinkskills, #intlinkskills, #clinkskills, #bklinkskills').on('click', function() {
     $('.startpage, .intpage, .contact, .mepage, .bookspage, .gamespage, .videospage, .audiospage').hide();
-    $('body').removeClass('mepage intpage contact startpage bookspage gamespage videospage audiospage');
-    $('body').addClass('skillspage');
+    $('body, header').removeClass('mepage intpage contact startpage bookspage gamespage videospage audiospage');
+    $('body, header').addClass('skillspage');
     $('.skillspage').show();
     $('title').text('Skills');
   });
   $('#intlinkbooks, #bklinkbooks').on('click', function() {
     $('.intpage, .gamespage, .videospage, .audiospage').hide();
-    $('body').removeClass('intpage gamespage videospage audiospage');
-    $('body').addClass('bookspage');
+    $('body, header').removeClass('intpage gamespage videospage audiospage');
+    $('body, header').addClass('bookspage');
     $('.bookspage').show();
     $('title').text('Books');
   });
   $('#intlinkgames, #bklinkgames').on('click', function() {
     $('.intpage, .bookspage, .videospage, .audiospage').hide();
-    $('body').removeClass('intpage bookspage videospage audiospage');
-    $('body').addClass('gamespage');
+    $('body, header').removeClass('intpage bookspage videospage audiospage');
+    $('body, header').addClass('gamespage');
     $('.gamespage').show();
     $('title').text('Games');
   });
   $('#intlinkvideos, #bklinkvideos').on('click', function() {
     $('.intpage, .bookspage, .gamespage, .audiospage').hide();
-    $('body').removeClass('intpage bookspage gamespage audiospage');
-    $('body').addClass('videospage');
+    $('body, header').removeClass('intpage bookspage gamespage audiospage');
+    $('body, header').addClass('videospage');
     $('.videospage').show();
     $('title').text('Videos');
   });
   $('#intlinkmusic, #bklinkmusic').on('click', function() {
     $('.intpage, .bookspage, .gamespage, .videospage').hide();
-    $('body').removeClass('intpage bookspage gamespage videospage');
-    $('body').addClass('audiospage');
+    $('body, header').removeClass('intpage bookspage gamespage videospage');
+    $('body, header').addClass('audiospage');
     $('.audiospage').show();
     $('title').text('Music');
   });
